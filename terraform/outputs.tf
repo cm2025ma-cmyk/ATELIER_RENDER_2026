5e0b084807a9febd039b68be3620eaa497a23ea0
cm2025ma-cmyk <cholinmatthieu@gmail.com>
2026-04-01 15:51:56 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
 output "service_name" {
   value = render_web_service.flask_app.name
 }
+
+output "flask_url" {
+  value       = render_web_service.flask_app.url
+  description = "URL du service Flask"
+}
+
+output "adminer_url" {
+  value       = render_web_service.adminer.url
+  description = "URL du service Adminer"
+}
+
+output "db_connection_string" {
+  value       = render_postgres.db.connection_info.external_connection_string
+  description = "PostgreSQL connection string"
+  sensitive   = true
+}
